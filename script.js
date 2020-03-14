260c7da50e96d6bebb366c1e2d4b3d4d0dd50d16
ROnic404 <ronic@inbox.ru>
2020-03-14 18:06:37 +0400

    feat: add script.js without slider task

---
+++ script.js
@@ -0,0 +1,78 @@
+window.onload = function() {
+    addHeaderNavigationClickHandler();
+    addPhoneButtonClickHandler();
+    addPortfolioTagsClickHandler();
+    addPortfolioImagesClickHandler();
+    addContactsFormSendHandler();
+}
+
+const addHeaderNavigationClickHandler = () => {
+    document.querySelector('.header .navigation').addEventListener('click', (item) => {
+        if(item.target.classList.contains('navigation__item')) {
+            document.querySelectorAll('.header .navigation__item').forEach(item => {
+                item.classList.remove('navigation__item_active');
+            });
+            item.target.classList.add('navigation__item_active');
+        }
+    })
+}
+
+const addPhoneButtonClickHandler = () => {
+    document.querySelector('.button-verphone').addEventListener('click', () => {
+        document.querySelector('.black-screen__verphone').classList.toggle('hidden');
+    });
+
+    document.querySelector('.button-gorphone').addEventListener('click', () => {
+        document.querySelector('.black-screen__gorphone').classList.toggle('hidden');
+    });
+}
+
+const addPortfolioTagsClickHandler = () => {
+    document.querySelector('.portfolio__list').addEventListener('click', (item) => {
+        if(item.target.classList.contains('portfolio__item')) {
+            document.querySelectorAll('.portfolio__item').forEach(item => {
+                item.classList.remove('portfolio__item_active');
+            });
+            item.target.classList.add('portfolio__item_active');
+            document.querySelector('.portfolio__images').appendChild(document.querySelector('.portfolio__image'))
+        }
+    })
+}
+
+const addPortfolioImagesClickHandler = () => {
+    document.querySelector('.portfolio__images').addEventListener('click', (image) => {
+        if(image.target.classList.contains('portfolio__image')) {
+            document.querySelectorAll('.portfolio__image').forEach(image => {
+                if(image.classList.contains('portfolio__image_active')) image.classList.remove('portfolio__image_active');
+            });
+            image.target.classList.toggle('portfolio__image_active');
+        }
+    })
+}
+
+const addContactsFormSendHandler = () => {
+    document.querySelector('.form button').addEventListener('click', (event) => {
+        if(document.querySelector('#name').value !== '' && document.querySelector('#email').value !== '') {
+            event.preventDefault();
+
+            const subject = document.querySelector('#subject').value.toString();
+            const describe = document.querySelector('#describe').value.toString();
+            let text_1 = document.querySelector('.text-1');
+            let text_2 = document.querySelector('.text-2');
+            let text_3 = document.querySelector('.text-3');
+
+            document.querySelector('.message-block').classList.remove('message-block-hidden');
+            document.body.style.overflow = "hidden";
+            document.body.style.paddingRight = "15px";
+            text_1.innerText = 'Письмо отправлено';
+            (subject) ? text_2.innerText = `Тема: ${subject}` : text_2.innerText = 'Без темы';
+            (describe) ? text_3.innerText = `Описание: ${describe}` : text_3.innerText = 'Без описания';
+        }   
+    });
+
+    document.querySelector('#close-button').addEventListener('click', () => {
+        document.querySelector('.message-block').classList.add('message-block-hidden');
+        document.body.style.overflow = "";
+        document.body.style.paddingRight = "";
+    })
+}
